dc73ea571343a54c61cceb3ed691d6120d35d540
Tsironis Ioannis <gtsironis8@gmail.com>
2025-12-01 14:12:17 +0200

    add theme support

--- apps/ecommerce/src/app/app.config.ts
+++ apps/ecommerce/src/app/app.config.ts
@@ -9,10 +9,87 @@ import {
   withInterceptorsFromDi,
 } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
+import { definePreset } from '@primeuix/themes';
 
+import { providePrimeNG } from 'primeng/config';
 import { appRoutes } from './app.routes';
+
+const MyPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{indigo.50}',
+      100: '{indigo.100}',
+      200: '{indigo.200}',
+      300: '{indigo.300}',
+      400: '{indigo.400}',
+      500: '{indigo.500}',
+      600: '{indigo.600}',
+      700: '{indigo.700}',
+      800: '{indigo.800}',
+      900: '{indigo.900}',
+      950: '{indigo.950}',
+    },
+    colorScheme: {
+      light: {
+        primary: {
+          color: '{primary.950}',
+          contrastColor: '#ffffff',
+          hoverColor: '{primary.800}',
+          activeColor: '{primary.700}',
+        },
+        highlight: {
+          background: '{primary.950}',
+          focusBackground: '{primary.700}',
+          color: '#ffffff',
+          focusColor: '#ffffff',
+        },
+        surface: {
+          0: '#ffffff',
+          50: '{zinc.50}',
+          100: '{zinc.100}',
+          200: '{zinc.200}',
+          300: '{zinc.300}',
+          400: '{zinc.400}',
+          500: '{zinc.500}',
+          600: '{zinc.600}',
+          700: '{zinc.700}',
+          800: '{zinc.800}',
+          900: '{zinc.900}',
+          950: '{zinc.950}',
+        },
+      },
+      dark: {
+        primary: {
+          color: '{primary.50}',
+          contrastColor: '{primary.950}',
+          hoverColor: '{primary.200}',
+          activeColor: '{primary.300}',
+        },
+        highlight: {
+          background: '{primary.50}',
+          focusBackground: '{primary.300}',
+          color: '{primary.950}',
+          focusColor: '{primary.950}',
+        },
+        surface: {
+          0: '#ffffff',
+          50: '{slate.50}',
+          100: '{slate.100}',
+          200: '{slate.200}',
+          300: '{slate.300}',
+          400: '{slate.400}',
+          500: '{slate.500}',
+          600: '{slate.600}',
+          700: '{slate.700}',
+          800: '{slate.800}',
+          900: '{slate.900}',
+          950: '{slate.950}',
+        },
+      },
+    },
+  },
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -23,7 +100,12 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Aura,
+        preset: MyPreset,
+        options: {
+          options: {
+            darkModeSelector: '.ecommerce-app-dark',
+          },
+        },
       },
     }),
   ],
--- apps/ecommerce/src/app/layout/header/header.component.ts
+++ apps/ecommerce/src/app/layout/header/header.component.ts
@@ -1,11 +1,33 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  effect,
+  signal,
+} from '@angular/core';
 
 import { CartIconComponent } from './cart-icon/cart-icon.component';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CartIconComponent],
+  imports: [CartIconComponent, ButtonModule],
+  host: {
+    class: 'flex bg-white dark:bg-gray-900 h-20 fixed top-0 left-0 z-50 w-full',
+  },
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  protected readonly theme = signal<'light' | 'dark'>('light');
+
+  readonly onThemeChange = effect(() => {
+    document.documentElement.classList.toggle(
+      'ecommerce-app-dark',
+      this.theme() === 'dark'
+    );
+  });
+
+  public toggleTheme(): void {
+    this.theme.update((theme) => (theme === 'light' ? 'dark' : 'light'));
+  }
+}
