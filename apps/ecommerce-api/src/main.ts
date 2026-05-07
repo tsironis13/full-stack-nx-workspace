ad72b3a86191336776604640157abdb62284da83
Tsironis Ioannis <gtsironis8@gmail.com>
2026-05-07 13:54:21 +0300

    fix api config

--- apps/ecommerce-api/src/main.ts
+++ apps/ecommerce-api/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3001;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
